499103958f0c99fc3eb33307af49fdcc459cd39a
astro8891 <ashley@equinox.co.nz>
2016-05-26 17:17:30 +1200

    init

---
+++ RaysHotDogs/RayHotDogs.Core/Repository/HotDogRepository.cs
@@ -0,0 +1,38 @@
+﻿using RayHotDogs.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayHotDogs.Core.Repository
+{
+    class HotDogRepository
+    {
+        private static List<HotDogGroup> hotDogGroups = new List<HotDogGroup>()
+        {
+             new HotDogGroup()
+             {
+                 HotDogGroupId = 1, Title = "Meat Lovers", ImagePath = "", HotDogs = new List<HotDog>()
+                 {
+                     new HotDog()
+                     {
+                         HotDogId = 1,
+                         Name = "Regular Hot Dog",
+                         ShortDescription = "The best there is on this planet",
+                         Description = "Manchego smelly cheese danish fontina",
+                         ImagePath = "hotdog1",
+                         Available = true,
+                         PrepTime = 10,
+                         Price =8,
+                         IsFavorite = true
+                     },
+                     new HotDog()
+                     {
+
+                     }
+                 } 
+
+        }
+    }
+}
